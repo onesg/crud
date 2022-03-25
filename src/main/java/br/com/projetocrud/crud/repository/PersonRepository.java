17414ff8c2e1267dd5ae200f614e85b761d4fc0a
onesg <gabriel.seno@fatec.sp.gov.br>
2022-03-24 21:39:24 -0300

    Adicionado notação @Repository

--- src/main/java/br/com/projetocrud/crud/repository/PersonRepository.java
+++ src/main/java/br/com/projetocrud/crud/repository/PersonRepository.java
@@ -2,9 +2,13 @@ package br.com.projetocrud.crud.repository;
 
 import br.com.projetocrud.crud.model.PersonModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/* TRANSFORMANDO A CLASSE EM UM REPOSITÓRIO */
+@Repository
 
 public interface PersonRepository extends JpaRepository<PersonModel, Long> {
 
-
+    /* POR TRÁS DOS PANOS IRÁ IMPLEMENTAR TODAS AS OPERAÇÕES BÁSICAS DO BANCO DE DADOS */
 
 }
