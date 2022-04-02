095b1f28a7605803f3dd4caa5de2b6bb6043ff2b
onesg <gabriel.seno@fatec.sp.gov.br>
2022-04-01 22:19:10 -0300

    Implementado VehicleRepository

--- src/main/java/br/com/projetocrud/crud/repository/VehicleRepository.java
+++ src/main/java/br/com/projetocrud/crud/repository/VehicleRepository.java
@@ -1,4 +1,14 @@
 package br.com.projetocrud.crud.repository;
 
-public interface VehicleRepository {
+import br.com.projetocrud.crud.model.VehicleModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/* TRANSFORMANDO A CLASSE EM UM REPOSITÓRIO */
+@Repository
+
+public interface VehicleRepository extends JpaRepository<VehicleModel, Long> {
+
+    /* TODAS AS OPERAÇÕES BÁSICAS DO BANCO DE DADOS SERÃO IMPLEMENTADAS */
+
 }
