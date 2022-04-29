033891dab5e8d2c516dbef31a6b5398db9b358e8
onesg <gabriel.seno@fatec.sp.gov.br>
2022-04-28 22:11:13 -0300

    Atualizando Swagger

--- src/main/java/br/com/projetocrud/crud/controller/PersonControllerV2.java
+++ src/main/java/br/com/projetocrud/crud/controller/PersonControllerV2.java
@@ -1,12 +1,19 @@
 package br.com.projetocrud.crud.controller;
 
+import br.com.projetocrud.crud.model.PersonModel;
 import br.com.projetocrud.crud.model.PersonModelV2;
 import br.com.projetocrud.crud.service.PersonServiceV2;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+/**/
+@Api(value = "this API manipulates Person", tags = {"Person","CRUD","REST"})
 
 /* SERÁ UM CONTROLADOR REST (MESMA LÓGICA DAS SERVLETS) */
 @RestController
@@ -22,11 +29,19 @@ public class PersonControllerV2 {
     /* DECLARANDO O SERVIÇO service */
     private PersonServiceV2 service;
 
+    /**/
+    @ApiOperation(value = "Retrive a Person using an ID", produces = "JSON", response = PersonModel.class)
+
     /* QUALQUER REQUISIÇÃO VIA GET PARA /person/<ID> SERÁ DIRECIONADO PARA ESSE MÉTODO */
     @GetMapping("/{id}")
-    public PersonModelV2 findbyId(@PathVariable("id") long id) throws Exception{
+    public PersonModelV2 findbyId(
+            @ApiParam(name = "id", value = "An Integer value", required = true)
+            @PathVariable("id") long id) throws Exception{
         return service.findbyId(id);
     }
+
+    /**/
+    @ApiOperation(value = "Return a list af People", produces = "JSON", response = PersonModel.class)
 
     /* CASO NÃO TENHA NADA DEPOIS DE /<ID> OS DADOS SERÃO LISTADOS NESTE MÉTODO */
     @GetMapping
